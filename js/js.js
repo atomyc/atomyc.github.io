3b76d048efe13e6035ca889cadd4a10411110899
Hector <hectoruch1@hotmail.com>
2016-05-08 18:24:37 +0200

    atomyc Beta 0.2
    Add Index, Palet with Headings, Paragraph, Visibility, Colors

---
+++ js/js.js
@@ -0,0 +1,6 @@
+$(window).resize(function () {
+	$(".headerimg").css('height', $(window).height()+"px");
+});
+$(document).ready(function () {
+	$(".headerimg").css('height', $(window).height()+"px");
+});
